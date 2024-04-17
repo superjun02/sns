38b52df73c9a7c78659a7806d2b11d462ac86ad9
DESKTOP-5MMQDH9\이상준 <superjun02@naver.com>
2024-04-17 19:06:45 +0900

    초기 환경 세팅

---
+++ src/main/java/com/sns/test/TestController.java
@@ -0,0 +1,45 @@
+package com.sns.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sns.post.mapper.PostMapper;
+
+@Controller
+public class TestController {
+	@Autowired
+	private PostMapper postMapper;
+	
+	@GetMapping("/test1")
+	@ResponseBody
+	public String test1() {
+		return "Hello world";
+	}
+	
+	@GetMapping("/test2")
+	@ResponseBody
+	public Map<String, Object> test2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("a", 12);
+		map.put("b", 22);
+		map.put("c", 32);
+		return map;
+	}
+	
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test3";
+	}
+	
+	@GetMapping("/test4")
+	@ResponseBody
+	public List<Map<String, Object>> test4() {
+		return postMapper.selectPostList();
+	}
+}
